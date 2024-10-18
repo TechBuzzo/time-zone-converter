1aa33703774c026898db6a98cac728a39f42c435
Max Bizera <mbizera@norelian.com>
2024-10-18 19:41:14 -0300

    feat: agregar función para convertir fechas a zona horaria y actualizar pruebas

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,11 +1,15 @@
-const convertTimeZone = require('../src/index');
+const { convertTimeAtZone, convertDateToZone } = require('../src/index');
 
 test('converts time from Montevideo to UTC', () => {
-  return convertTimeZone({
+  const result = convertTimeAtZone({
     timeZone: 'America/Montevideo',
     time: '15:00',
     baseDate: new Date('2024-09-12T12:00:00.000Z')
-  }).then(data => {
-    expect(data).toBe('2024-09-12T12:00:00.000Z');
   });
+  expect(result).toBe('2024-09-12T12:00:00.000Z');
+});
+
+test('converts date to Montevideo timezone', () => {
+  const result = convertDateToZone('America/Montevideo', new Date('2024-09-12T12:00:00.000Z'));
+  expect(result).toBe('2024-09-12T09:00:00.000Z'); // Resultado esperado basado en el cambio de zona horaria
 });
